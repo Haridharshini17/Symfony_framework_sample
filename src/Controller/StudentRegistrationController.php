0d7e2f53f8877037ef955a43bc1b53cdfde2b081
Haridharshini17 <haridha.senthil@aspiresys.com>
2022-10-10 01:06:33 +0530

    commit

--- src/Controller/StudentRegistrationController.php
+++ src/Controller/StudentRegistrationController.php
@@ -4,19 +4,31 @@ namespace App\Controller;
 
 use App\Form\Type\StudentFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use symfony\component\Routing\Annotation\Route;
-use symfony\component\HttpFoundation\Response;
-
+use Symfony\component\Routing\Annotation\Route;
+use Symfony\component\HttpFoundation\Response;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\StudentRegistration;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class StudentRegistrationController extends AbstractController
 {
-    #[Route('/exampleform' , name: 'exampleshow')]
-    public function new(): Response
+     #[Route('/exampleform' , name: 'exampleshow',)] //methods: ['POST'])]
+    public function new(ManagerRegistry $doctrine, Request $request,EntityManagerInterface $entityManager): Response
     { 
-        $formcreated = $this->createForm(StudentFormType::class);
+        $datas = new StudentRegistration();
+        $formcreated = $this->createForm(StudentFormType::class, $datas);
+        $formcreated->handleRequest($request);
+        if($formcreated->isSubmitted() && $formcreated->isValid())
+        {
+            $entityManager->persist($datas);
+            $entityManager->flush();
+            return new Response('sucess');
+        }
         return $this->renderForm('submitForm/new.html.twig',
         [
-            'formcreated' => $formcreated,
+            'formcreated' => $formcreated,//$datas->getId(),
         ]);
     }
 }
